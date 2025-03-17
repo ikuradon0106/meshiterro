9ac2c8ef4381165157acaa340aa87edd9d8cb89b
EC2 Default User <ec2-user@ip-172-31-40-77.ap-northeast-1.compute.internal>
2025-03-17 02:05:44 +0000

    [break]app2-18

--- app/controllers/show_controller.rb
+++
@@ -1,4 +0,0 @@
-class ShowController < ApplicationController
-  def edit
-  end
-end
--- db/migrate/20250316072159_create_post_comments.rb
+++ db/migrate/20250316072159_create_post_comments.rb
@@ -1,9 +1,9 @@
-class CreatePostComments < ActiveRecord::Migration[6.1]
+class CreatePostCommens < ActiveRecord::Migration[6.1]
   def change
     create_table :post_comments do |t|
       t.text :comment
       t.integer :user_id
-      t.integer :postimage_id
+      t.integer :post_image_id
 
       t.timestamps
     end
