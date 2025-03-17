b9020b37ed5297a34ffa45bc145dead88fd6edaf
EC2 Default User <ec2-user@ip-172-31-40-77.ap-northeast-1.compute.internal>
2025-03-17 04:50:59 +0000

    [finish]app2-19

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -7,6 +7,11 @@ class PostCommentsController < ApplicationController
       comment.save
       redirect_to post_image_path(post_image)
     end
+
+    def destroy
+      PostComment.find(params[:id]).destroy
+      redirect_to post_image_path(params[:post_image_id])
+    end
   
     private
   
@@ -15,4 +20,3 @@ class PostCommentsController < ApplicationController
     end
   
   end
-end
